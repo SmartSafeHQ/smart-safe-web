caa5b1316d4bc12b583d68a789226e6cc948341d
Paulo Reis <paulosilvadosreis2057@gmail.com>
2023-01-23 12:00:42 -0300

    build(styles): setup tailwind css to project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
