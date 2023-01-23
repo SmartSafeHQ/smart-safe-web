0fbc129bc2b5821223a0f14501e0b4213329d9cb
Paulo Reis <paulosilvadosreis2057@gmail.com>
2023-01-23 17:45:40 -0300

    feat(scroll-area): creat ScrollArea global component

---
+++ src/components/ScrollArea.tsx
@@ -0,0 +1,36 @@
+import { ReactNode } from 'react'
+import * as RadixScrollArea from '@radix-ui/react-scroll-area'
+import clsx from 'clsx'
+
+export interface ScrollAreaProps extends RadixScrollArea.ScrollAreaProps {
+  children: ReactNode
+}
+
+export function ScrollArea({ children, className, ...props }: ScrollAreaProps) {
+  return (
+    <RadixScrollArea.Root
+      className={clsx(`w-full overflow-hidden`, className)}
+      {...props}
+    >
+      <RadixScrollArea.Viewport className="w-full h-full flex flex-1 [&>div]:!block">
+        {children}
+      </RadixScrollArea.Viewport>
+
+      <RadixScrollArea.Scrollbar
+        className="flex select-none touch-none bg-gray-800 brightness-150 rounded-md data-[orientation=vertical]:w-1 data-[orientation=horizontal]:flex-col data-[orientation=horizontal]:h-1"
+        orientation="vertical"
+      >
+        <RadixScrollArea.Thumb className="flex-1 bg-gray-600 brightness-50 rounded-xl relative before:content-[''] before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:w-full before:h-full before:min-w-[2.75rem] before:min-h-[2.75rem]" />
+      </RadixScrollArea.Scrollbar>
+
+      <RadixScrollArea.Scrollbar
+        className="flex select-none touch-none bg-gray-800 brightness-150 rounded-md data-[orientation=vertical]:w-1 data-[orientation=horizontal]:flex-col data-[orientation=horizontal]:h-1"
+        orientation="horizontal"
+      >
+        <RadixScrollArea.Thumb className="flex-1 bg-gray-600 brightness-50 rounded-xl relative before:content-[''] before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:w-full before:h-full before:min-w-[2.75rem] before:min-h-[2.75rem]" />
+      </RadixScrollArea.Scrollbar>
+
+      <RadixScrollArea.Corner className="bg-gray-800" />
+    </RadixScrollArea.Root>
+  )
+}
