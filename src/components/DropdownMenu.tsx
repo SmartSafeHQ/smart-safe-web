7bc8ad09bfce4184178009c54a2531e8a5f00667
Paulo Reis <paulosilvadosreis2057@gmail.com>
2023-01-23 17:52:54 -0300

    feat(dropdown-menu): create global DropdownMenu component

---
+++ src/components/DropdownMenu.tsx
@@ -0,0 +1,117 @@
+import { ForwardRefExoticComponent, ReactNode, RefAttributes } from 'react'
+import { clsx } from 'clsx'
+import * as RadixDropdownMenu from '@radix-ui/react-dropdown-menu'
+import { IconProps } from 'phosphor-react'
+import { Slot } from '@radix-ui/react-slot'
+
+export interface DropdownMenuRootProps
+  extends RadixDropdownMenu.DropdownMenuProps {
+  children: ReactNode
+}
+
+function DropdownMenuRoot({ children, ...props }: DropdownMenuRootProps) {
+  return <RadixDropdownMenu.Root {...props}>{children}</RadixDropdownMenu.Root>
+}
+
+DropdownMenuRoot.displayName = 'RadixDropdownMenu.Root'
+
+export interface DropdownMenuTriggerProps {
+  children: ReactNode
+}
+
+function DropdownMenuTrigger({ children }: DropdownMenuTriggerProps) {
+  return (
+    <RadixDropdownMenu.Trigger asChild>{children}</RadixDropdownMenu.Trigger>
+  )
+}
+
+DropdownMenuTrigger.displayName = 'RadixDropdownMenu.Trigger'
+
+export interface DropdownMenuContentProps
+  extends RadixDropdownMenu.MenuContentProps {
+  children: ReactNode
+}
+
+function DropdownMenuContent({
+  children,
+  className,
+  ...props
+}: DropdownMenuContentProps) {
+  return (
+    <RadixDropdownMenu.Portal>
+      <RadixDropdownMenu.Content
+        className={clsx(
+          'w-[100vw] min-w-[15rem] pb-4 bg-gray-800 rounded-md shadow-md animate-dropdown-menu-open md:w-full',
+          className
+        )}
+        sideOffset={5}
+        {...props}
+      >
+        {children}
+      </RadixDropdownMenu.Content>
+    </RadixDropdownMenu.Portal>
+  )
+}
+
+DropdownMenuContent.displayName = 'RadixDropdownMenu.Content'
+
+export type DropdownMenuSeparatorProps = RadixDropdownMenu.MenuSeparatorProps
+
+function DropdownMenuSeparator({
+  className,
+  ...props
+}: DropdownMenuSeparatorProps) {
+  return (
+    <RadixDropdownMenu.Separator
+      className={clsx('h-[1px] bg-gray-700 my-1 mx-3', className)}
+      {...props}
+    />
+  )
+}
+
+DropdownMenuSeparator.displayName = 'RadixDropdownMenu.Separator'
+
+export interface DropdownMenuItemProps extends RadixDropdownMenu.MenuItemProps {
+  LeftIcon?: ForwardRefExoticComponent<IconProps & RefAttributes<SVGSVGElement>>
+  isDisabled?: boolean
+  children: ReactNode
+}
+
+function DropdownMenuItem({
+  children,
+  LeftIcon,
+  isDisabled = false,
+  className,
+  ...props
+}: DropdownMenuItemProps) {
+  return (
+    <RadixDropdownMenu.Item
+      className={clsx(
+        'relative flex items-center justify-start gap-4 px-6 py-3 select-none outline-none text-base font-medium capitalize text-cyan-50 cursor-pointer hover:bg-gray-900 hover:bg-opacity-40 ',
+        {
+          'pointer-events-none brightness-50': isDisabled
+        },
+        className
+      )}
+      {...props}
+    >
+      {LeftIcon && (
+        <Slot className="w-5 h-5 text-cyan-500">
+          <LeftIcon weight="fill" />
+        </Slot>
+      )}
+
+      {children}
+    </RadixDropdownMenu.Item>
+  )
+}
+
+DropdownMenuItem.displayName = 'RadixDropdownMenu.Item'
+
+export const DropdownMenu = {
+  Root: DropdownMenuRoot,
+  Trigger: DropdownMenuTrigger,
+  Content: DropdownMenuContent,
+  Separator: DropdownMenuSeparator,
+  Item: DropdownMenuItem
+}
