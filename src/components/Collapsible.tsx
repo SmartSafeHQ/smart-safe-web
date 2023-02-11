feeb5686446e6afd032f7b4e916f10c8f0afeb9b
Paulo Reis <paulosilvadosreis2057@gmail.com>
2023-02-10 23:48:55 -0300

    feat(collapsible): create Collapsible global component

---
+++ src/components/Collapsible.tsx
@@ -0,0 +1,67 @@
+import { ReactNode } from 'react'
+import * as RadixCollapsible from '@radix-ui/react-collapsible'
+import clsx from 'clsx'
+
+export interface CollapsibleRootProps
+  extends RadixCollapsible.CollapsibleProps {
+  children: ReactNode
+}
+
+function CollapsibleRoot({
+  children,
+  className,
+  ...props
+}: CollapsibleRootProps) {
+  return (
+    <RadixCollapsible.Root
+      className={clsx('w-full flex flex-col', className)}
+      {...props}
+    >
+      {children}
+    </RadixCollapsible.Root>
+  )
+}
+
+CollapsibleRoot.displayName = 'RadixCollapsible.Root'
+
+export interface CollapsibleTriggerProps
+  extends RadixCollapsible.CollapsibleTriggerProps {
+  children: ReactNode
+}
+
+function CollapsibleTrigger({
+  children,
+  className,
+  ...props
+}: CollapsibleTriggerProps) {
+  return (
+    <RadixCollapsible.Trigger
+      className={clsx('data-[state=open]:brightness-90', className)}
+      asChild
+      {...props}
+    >
+      {children}
+    </RadixCollapsible.Trigger>
+  )
+}
+
+CollapsibleTrigger.displayName = 'RadixCollapsible.Trigger'
+
+export interface CollapsibleContentProps
+  extends RadixCollapsible.CollapsibleContentProps {
+  children: ReactNode
+}
+
+function CollapsibleContent({ children, ...props }: CollapsibleContentProps) {
+  return (
+    <RadixCollapsible.Content {...props}>{children}</RadixCollapsible.Content>
+  )
+}
+
+CollapsibleContent.displayName = 'RadixCollapsible.Content'
+
+export const Collapsible = {
+  Root: CollapsibleRoot,
+  Trigger: CollapsibleTrigger,
+  Content: CollapsibleContent
+}
