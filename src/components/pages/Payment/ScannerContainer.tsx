06740b69003d2819361ddc7c8da4adf79c6ed958
Ricardo Passos <ricardo.passos.cr@gmail.com>
2023-02-19 15:10:37 -0300

    fix(payment): try to get the video stream to work on mobile by using native lib

--- src/components/pages/Payment/ScannerContainer.tsx
+++ src/components/pages/Payment/ScannerContainer.tsx
@@ -1,4 +1,10 @@
-import { useZxing } from 'react-zxing'
+import { useEffect, useRef } from 'react'
+// import { useZxing } from 'react-zxing'
+import {
+  BarcodeFormat,
+  DecodeHintType,
+  BrowserMultiFormatReader
+} from '@zxing/library'
 
 import { ScannerStrategy, QrCodeScanner } from './ScannerStrategy'
 
@@ -11,25 +17,57 @@ type Props = {
 }
 
 export function ScannerContainer({
-  setQrCodeDecodedData,
-  usersCameraDevices
-}: Props) {
-  const { ref } = useZxing({
-    onResult(codeData) {
-      const scannerStrategy = new ScannerStrategy(new QrCodeScanner())
+  setQrCodeDecodedData
+}: // usersCameraDevices
+Props) {
+  // const { ref } = useZxing({
+  //   onResult(codeData) {
+  //     const scannerStrategy = new ScannerStrategy(new QrCodeScanner())
 
-      const scanResult = scannerStrategy.scan(codeData.getText())
+  //     const scanResult = scannerStrategy.scan(codeData.getText())
 
-      setQrCodeDecodedData(scanResult as QrCodeData)
-    },
-    deviceId:
-      usersCameraDevices.backCameraId || usersCameraDevices.frontCameraId
-  })
+  //     setQrCodeDecodedData(scanResult as QrCodeData)
+  //   },
+  //   deviceId:
+  //     usersCameraDevices.backCameraId || usersCameraDevices.frontCameraId
+  // })
+
+  const videoStream = useRef<HTMLVideoElement>(null)
+
+  useEffect(() => {
+    window.navigator.mediaDevices
+      .getUserMedia({
+        video: { facingMode: 'environment' }
+      })
+      .then(mediaStream => {
+        const hints = new Map()
+        const formats = [BarcodeFormat.QR_CODE]
+        hints.set(DecodeHintType.POSSIBLE_FORMATS, formats)
+
+        const reader = new BrowserMultiFormatReader(hints)
+
+        reader.timeBetweenDecodingAttempts = 1000
+
+        reader.decodeFromStream(
+          mediaStream,
+          videoStream.current as HTMLVideoElement,
+          result => {
+            if (result) {
+              const scannerStrategy = new ScannerStrategy(new QrCodeScanner())
+
+              const scanResult = scannerStrategy.scan(result.getText())
+
+              setQrCodeDecodedData(scanResult as QrCodeData)
+            }
+          }
+        )
+      })
+  }, [])
 
   return (
     <div className="rounded-lg overflow-hidden w-64 h-64 sm:w-96">
       <video
-        ref={ref}
+        ref={videoStream}
         autoPlay={true}
         className="border-1 border-red-500 w-64 h-64"
       />
