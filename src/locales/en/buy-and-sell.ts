7e096565275b37fbda896596b1829f48525fb775
Paulo Reis <paulosilvadosreis2057@gmail.com>
2023-03-28 13:58:13 -0300

    refactor(buy-sell): refact buy and sell page into components and add i18n

--- src/locales/en/buy-and-sell.ts
+++ src/locales/en/buy-and-sell.ts
@@ -1,4 +1,19 @@
 export const buyAndSell = {
   headTitle: 'InWallet | Buy and sell',
-  headDescription: 'Make payments using your cryptocurrencies.'
+  headDescription: 'Efetue pagamentos usando suas crypto moedas.',
+  selectAct: {
+    title: 'Select buy/sell',
+    buyButton: 'Buy Now',
+    sellButton: 'Sell Now',
+    buyTitle: 'Buy process',
+    sellTitle: 'Selling process',
+    buyDesc01: '1. Enter your wallet adress',
+    buyDesc02:
+      '2. Wire funds from your bank account, using the wiring instructions provided.',
+    buyDesc03: '3. The Coin will be sent to your wallet address.',
+    sellDesc01: '1. Add the bank account you will to receive funds.',
+    sellDesc02:
+      '2. Send coin to your redemption address, using the provided instructions.',
+    sellDesc03: '3. The Coin will be deposited to your bank account.'
+  }
 }
