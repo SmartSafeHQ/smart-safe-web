a3751a60c02a66e67e0d37727b6ae30ec55c7174
Ricardo Passos <ricardo.passos.cr@gmail.com>
2023-04-18 16:21:11 -0300

    fix: typo

--- src/hooks/buyAndSell/mutations/useBurnStableCoin.ts
+++ src/hooks/buyAndSell/mutations/useBurnStableCoin.ts
@@ -16,7 +16,7 @@ async function burnStableCoin({
   contractAddress
 }: MintStableCoinPayload): Promise<string> {
   const { data: transactionHash } = await tokenverseApi.post(
-    '/contract/burnStableCoin',
+    '/contracts/burnStableCoin',
     {
       amount,
       userAddress,
