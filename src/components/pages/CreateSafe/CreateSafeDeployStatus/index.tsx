1b5476f5ad6a7cad4919e6ef091aa39a92c02d30
Paulo Reis <paulosilvadosreis2057@gmail.com>
2023-06-05 09:59:29 -0300

    fix(create-safe): fix deploy safe page mobile responsivity issues

--- src/components/pages/CreateSafe/CreateSafeDeployStatus/index.tsx
+++ src/components/pages/CreateSafe/CreateSafeDeployStatus/index.tsx
@@ -15,7 +15,7 @@ export function CreateSafeDeployStatus() {
   return (
     <article
       className={clsx(
-        'min-w-[23.25rem] min-h-[23rem] flex flex-col flex-1 items-stretch justify-start gap-6 px-6 pt-6 pb-12 relative rounded-md border-2 border-zinc-200 dark:border-zinc-700 bg-white dark:bg-black lg:p-8',
+        'min-h-[23rem] flex flex-col flex-1 items-stretch justify-start gap-6 px-6 pt-6 pb-12 relative rounded-md border-2 border-zinc-200 dark:border-zinc-700 bg-white dark:bg-black lg:p-8',
         {
           'border-zinc-200 dark:border-zinc-700 brightness-90 dark:brightness-50':
             deployStatus.steps.length <= 1
